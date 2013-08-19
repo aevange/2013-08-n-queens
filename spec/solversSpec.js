d44ecbc5834bc3927d1a8aa9722142c212ee3aab
aevange <andre.evangelesta@gmail.com>
2013-08-18 20:13:16 -0700

    Add more solutions in order to compare countNQueensSolutions running time.

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -36,11 +36,13 @@ describe("solvers", function() {
   });
 
   describe('countNQueensSolutions()', function(){
+    //some of the correct solutions are listed below
+    //1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184, 14772512, 95815104, 666090624, 4968057848, 39029188884, 314666222712, 2691008701644, 24233937684440, 227514171973736, 2207893435808352, 22317699616364044
 
-    it('finds the number of valid solutions for n of 0-8', function(){
-      _.range(0, 8).map(function(n){
+    it('finds the number of valid solutions for n', function(){
+      _.range(0, 13).map(function(n){
         var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596, 2279184, 14772512, 95815104, 666090624, 4968057848, 39029188884, 314666222712, 2691008701644, 24233937684440, 227514171973736, 2207893435808352, 22317699616364044][n];
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
